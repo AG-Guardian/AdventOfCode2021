b05b3e9691af7b68e311793fdd42ea65de9482d4
AG-Guardian <zachary.a.greer@gmail.com>
2021-12-06 09:25:32 -0600

    day 6 small perf improvement for big datasets

--- 6-2.py
+++ 6-2.py
@@ -1,10 +1,15 @@
 with open('in.txt', 'r') as file:
     fishes = [int(day) for day in file.read().split(',')]
 
-fish_by_day = [fishes.count(day) for day in range(9)]
+fish_by_day = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+for day in fishes:
+    fish_by_day[day] += 1
 
 for day in range(256):
+    # shift up the populations for each day
     fish_by_day.append(fish_by_day.pop(0))
+    # add back in the mothers at day 6 that were just converted to fresh spawn at day 8
     fish_by_day[6] += fish_by_day[8]
 
 print(sum(fish_by_day))
