010fe26ebdbe4ecf587c2a8613ae7c502fc95426
AG-Guardian <zachary.a.greer@gmail.com>
2021-12-06 09:27:52 -0600

    day 6 comments for clarity

--- 6-2.py
+++ 6-2.py
@@ -7,7 +7,7 @@ for day in fishes:
     fish_by_day[day] += 1
 
 for day in range(256):
-    # shift up the populations for each day
+    # shift up the populations for each day (decrementing the timer)
     fish_by_day.append(fish_by_day.pop(0))
     # add back in the mothers at day 6 that were just converted to fresh spawn at day 8
     fish_by_day[6] += fish_by_day[8]
