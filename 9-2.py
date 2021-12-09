8f6fa5e744cd6f3a42e44d6cf58b39550f7d30c7
AG-Guardian <zachary.a.greer@gmail.com>
2021-12-09 10:31:46 -0600

    day 9 optimized

--- 9-2.py
+++ 9-2.py
@@ -1,68 +1,65 @@
+class Point:
+    def __init__(self, x: int, y: int, val: int, next=None):
+        self.x = x
+        self.y = y
+        self.val = val
+        self.next = next
+
+
 with open('in.txt', 'r') as file:
     lines = file.read().splitlines()
 
-# set up the grid as a 2d array of ints
-grid = [[int(lines[y][x]) for x in range(len(lines[0]))] for y in range(len(lines))]
+# set up the grid as a 2d array of points
+grid = [[Point(x, y, int(lines[y][x])) for x in range(len(lines[0]))] for y in range(len(lines))]
 basins = dict([])
+
+top = 0
+left = 0
+bottom = len(grid) - 1
+right = len(grid[0]) - 1
 
 # determine the low points in the map
 for y in range(len(grid)):
     for x in range(len(grid[0])):
-        val = grid[y][x]
+        val = grid[y][x].val
 
         # check the (potential) 4 adjacent points to see if any are lower
-        if x < len(grid[y]) - 1:
-            if grid[y][x + 1] <= val:
-                continue
-        if x > 0:
-            if grid[y][x - 1] <= val:
-                continue
-        if y < len(grid) - 1:
-            if grid[y + 1][x] <= val:
-                continue
-        if y > 0:
-            if grid[y - 1][x] <= val:
-                continue
+        if val != 9:
+            # Check right
+            if x < right:
+                if grid[y][x + 1].val < val:
+                    val = grid[y][x + 1].val
+                    grid[y][x].next = grid[y][x + 1]
+            # Check left
+            if x > left:
+                if grid[y][x - 1].val < val:
+                    val = grid[y][x - 1].val
+                    grid[y][x].next = grid[y][x - 1]
+            # Check down
+            if y < bottom:
+                if grid[y + 1][x].val < val:
+                    val = grid[y + 1][x].val
+                    grid[y][x].next = grid[y + 1][x]
+            # Check up
+            if y > top:
+                if grid[y - 1][x].val < val:
+                    val = grid[y - 1][x].val
+                    grid[y][x].next = grid[y - 1][x]
 
-        # if we made it here, this is a low point. Add an entry for this basin
-        basins[f'{x},{y}'] = 0
+            # if this point does not flow anywhere, it's a low point. Add an entry for this basin to track its size
+            if not grid[y][x].next:
+                basins[f'{x},{y}'] = 0
 
 # for each point, find the low point it flows into and add 1 to the size of that basin
 for y in range(len(grid)):
     for x in range(len(grid[0])):
-        val = grid[y][x]
-        if val != 9:
-            x1 = x
-            y1 = y
-
-            # follow the flow towards the lowest point until we hit the low point in a basin
-            while f'{x1},{y1}' not in basins.keys():
-                adjacent = []
-                if x1 < len(grid[y1]) - 1:
-                    if grid[y1][x1 + 1] < val:
-                        y2 = y1
-                        x2 = x1 + 1
-                        val = grid[y2][x2]
-                if x1 > 0:
-                    if grid[y1][x1 - 1] < val:
-                        y2 = y1
-                        x2 = x1 - 1
-                        val = grid[y2][x2]
-                if y1 < len(grid) - 1:
-                    if grid[y1 + 1][x1] < val:
-                        y2 = y1 + 1
-                        x2 = x1
-                        val = grid[y2][x2]
-                if y1 > 0:
-                    if grid[y1 - 1][x1] < val:
-                        y2 = y1 - 1
-                        x2 = x1
-                        val = grid[y2][x2]
-                x1 = x2
-                y1 = y2
+        point = grid[y][x]
+        if point.val != 9:
+            while point.next:
+                point = point.next
 
             # once we have determined where this point flows to, we can increase its basin's size
-            basins[f'{x1},{y1}'] += 1
+            basins[f'{point.x},{point.y}'] += 1
 
 # print out the product of the 3 largest basins
 sorted_basins = list(reversed(sorted(basins.values())))
