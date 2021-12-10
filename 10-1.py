405fd571b078fea60d2f3368c733e54aa6b386ba
AG-Guardian <zachary.a.greer@gmail.com>
2021-12-10 00:18:27 -0600

    day 10

---
+++ 10-1.py
@@ -0,0 +1,47 @@
+closings = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+    '<': '>'
+}
+
+openings = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+    '>': '<'
+}
+
+points = {
+    ')': 3,
+    ']': 57,
+    '}': 1197,
+    '>': 25137
+}
+
+
+def stack_em_up(line: str):
+    stack = []
+    for char in line:
+        if char in openings.values():
+            stack.append(char)
+        elif char in closings.values():
+            if stack and openings[char] == stack[-1]:
+                stack.pop()
+            else:
+                return char
+        else:
+            print("Bad input.")
+    return None
+
+
+with open('in.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+total = 0
+for line in lines:
+    error = stack_em_up(line)
+    if error:
+        total += points[error]
+
+print(total)
---
+++ 10-2.py
@@ -0,0 +1,54 @@
+closings = {
+    '{': '}',
+    '[': ']',
+    '(': ')',
+    '<': '>'
+}
+
+openings = {
+    '}': '{',
+    ']': '[',
+    ')': '(',
+    '>': '<'
+}
+
+points = {
+    ')': 1,
+    ']': 2,
+    '}': 3,
+    '>': 4
+}
+
+
+def stack_em_up(line: str):
+    score = 0
+    stack = []
+
+    for char in line:
+        if char in openings.values():
+            stack.append(char)
+        elif char in closings.values():
+            if stack and openings[char] == stack[-1]:
+                stack.pop()
+            else:
+                return None
+        else:
+            print("Bad input.")
+
+    while stack:
+        score *= 5
+        score += points[closings[stack.pop()]]
+
+    return score
+
+
+with open('in.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+scores = []
+for line in lines:
+    score = stack_em_up(line)
+    if score:
+        scores.append(score)
+
+print(sorted(scores)[len(scores) // 2])
